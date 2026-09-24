e527e528e51ebd948973a5d7012bdeb24bd424dd
arielsepang93-commits <arielsepang93@gmail.com>
2026-09-24 12:26:28 +0800

    Add user storage and HTML escaping function

---
+++ api/users.js
@@ -0,0 +1,9 @@
+// Shared penyimpanan user (in-memory)
+export const users = new Map();
+
+export function escapeHtml(str) {
+    return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+}

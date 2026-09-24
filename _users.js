be7dcb5d4af33d63203a4fa838fed96df1c9cf93
arielsepang93-commits <arielsepang93@gmail.com>
2026-09-24 12:25:50 +0800

    Delete _users.js

--- _users.js
+++
@@ -1,9 +0,0 @@
-// Shared penyimpanan user (in-memory)
-export const users = new Map();
-
-export function escapeHtml(str) {
-    return String(str)
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
-}
